b5c448df30bbe310c23b39377beeace818fb358d
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-06-28 21:42:18 -0400

    Updates

---
+++ precache-manifest.93f5932e911984953ea4410b1b9d0270.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d8e867b4f0902e0cfbf7a324b49b98",
+    "url": "/tamago-sushi-bar/index.html"
+  },
+  {
+    "revision": "8fca7466726d57be3a42",
+    "url": "/tamago-sushi-bar/static/css/main.fe2941bd.chunk.css"
+  },
+  {
+    "revision": "7ebebf6ab1d8038251ed",
+    "url": "/tamago-sushi-bar/static/js/2.5e8a6568.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/tamago-sushi-bar/static/js/2.5e8a6568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fca7466726d57be3a42",
+    "url": "/tamago-sushi-bar/static/js/main.c147dd02.chunk.js"
+  },
+  {
+    "revision": "2ac46b9b2fe9f48bd793",
+    "url": "/tamago-sushi-bar/static/js/runtime-main.c96357f9.js"
+  },
+  {
+    "revision": "f0ed965587fcc4dafa08d8847862b63c",
+    "url": "/tamago-sushi-bar/static/media/Agedashi Tofu.f0ed9655.jpg"
+  },
+  {
+    "revision": "d69d048de2544b81cc8cd6fede62ceb1",
+    "url": "/tamago-sushi-bar/static/media/Amaebi.d69d048d.jpg"
+  },
+  {
+    "revision": "95d71536371f8e8b658af8fcd3082f95",
+    "url": "/tamago-sushi-bar/static/media/Avocado Salad.95d71536.png"
+  },
+  {
+    "revision": "9803107e876808c75b0645e2eb1962db",
+    "url": "/tamago-sushi-bar/static/media/Chicken Udon.9803107e.jpg"
+  },
+  {
+    "revision": "71b1225eeb6ccfda10a559ff355ed00d",
+    "url": "/tamago-sushi-bar/static/media/Edamame.71b1225e.jpg"
+  },
+  {
+    "revision": "1c071b0eaa1158e766957d975e55748d",
+    "url": "/tamago-sushi-bar/static/media/Green Kaiso Salad.1c071b0e.jpg"
+  },
+  {
+    "revision": "23f13ea26868590e0c6e3c548baf0d8c",
+    "url": "/tamago-sushi-bar/static/media/Hokkigai.23f13ea2.jpg"
+  },
+  {
+    "revision": "db4d0d09a91edd801a9d18fe1ac24a06",
+    "url": "/tamago-sushi-bar/static/media/Kaiso Udon.db4d0d09.jpg"
+  },
+  {
+    "revision": "1780f4d727bea30bc1064b726ce2b31a",
+    "url": "/tamago-sushi-bar/static/media/Kaki-Age-Don.1780f4d7.jpg"
+  },
+  {
+    "revision": "7264f230b6b5531f63d65efd20ea6175",
+    "url": "/tamago-sushi-bar/static/media/Kara-Age.7264f230.jpg"
+  },
+  {
+    "revision": "8af62a4db3b1adff4673e2db8985d4c8",
+    "url": "/tamago-sushi-bar/static/media/Katsu-Don.8af62a4d.jpg"
+  },
+  {
+    "revision": "ad5033d8d6db713c8f1c39c9063c07b5",
+    "url": "/tamago-sushi-bar/static/media/Maguro.ad5033d8.jpg"
+  },
+  {
+    "revision": "97d0981389ce8039ce5e9b1703ee719d",
+    "url": "/tamago-sushi-bar/static/media/Oyako-Don.97d09813.jpg"
+  },
+  {
+    "revision": "a352b2eb522c45fc20ae6083967e5753",
+    "url": "/tamago-sushi-bar/static/media/Sake.a352b2eb.jpg"
+  },
+  {
+    "revision": "1d53398307f6a65d18e2de23677e0044",
+    "url": "/tamago-sushi-bar/static/media/Tempura-Don.1d533983.jpg"
+  },
+  {
+    "revision": "d70a16d5da30f5723e705196cac4ac1f",
+    "url": "/tamago-sushi-bar/static/media/Unagi-Don.d70a16d5.jpg"
+  },
+  {
+    "revision": "40752869a2fa641fecfaae4c78d77817",
+    "url": "/tamago-sushi-bar/static/media/Wakame Salad.40752869.jpg"
+  },
+  {
+    "revision": "7919392b46102ac13d45d7773a5e8570",
+    "url": "/tamago-sushi-bar/static/media/Yaki Udon.7919392b.jpg"
+  },
+  {
+    "revision": "38c106f77cc75067efb908fc34048e78",
+    "url": "/tamago-sushi-bar/static/media/Yaki-Nasu.38c106f7.jpg"
+  },
+  {
+    "revision": "146e94df97ba4257973ff4e2fbbb63d5",
+    "url": "/tamago-sushi-bar/static/media/better.146e94df.svg"
+  },
+  {
+    "revision": "b45f252a39b8a45c28db209710cfbeab",
+    "url": "/tamago-sushi-bar/static/media/bg-2.b45f252a.jpg"
+  },
+  {
+    "revision": "c47e661e8cb0960c5215c3035de480d8",
+    "url": "/tamago-sushi-bar/static/media/butterfly.c47e661e.jpg"
+  },
+  {
+    "revision": "d14cd9f669b15b39f07704219389cca5",
+    "url": "/tamago-sushi-bar/static/media/ebi.d14cd9f6.jpg"
+  },
+  {
+    "revision": "186a888820ea4f7945d9778c25ba77cb",
+    "url": "/tamago-sushi-bar/static/media/family.186a8888.svg"
+  },
+  {
+    "revision": "a6cd9628b62e44b94fa1e79e591abe7f",
+    "url": "/tamago-sushi-bar/static/media/fresh.a6cd9628.svg"
+  },
+  {
+    "revision": "d7357e2d0fc9ec14e3fe09f950aea9ad",
+    "url": "/tamago-sushi-bar/static/media/hamachi.d7357e2d.jpg"
+  },
+  {
+    "revision": "d60a80e1cd38fd54383448999812b704",
+    "url": "/tamago-sushi-bar/static/media/iceCream.d60a80e1.jpg"
+  },
+  {
+    "revision": "7a75625d0554d152e9693f4c148dea0d",
+    "url": "/tamago-sushi-bar/static/media/ikura.7a75625d.jpg"
+  },
+  {
+    "revision": "faae76a6b749434bcec4eaa33cfc5668",
+    "url": "/tamago-sushi-bar/static/media/juice.faae76a6.jpg"
+  },
+  {
+    "revision": "45e095b67fff1414831488c1459c004f",
+    "url": "/tamago-sushi-bar/static/media/logo.45e095b6.svg"
+  },
+  {
+    "revision": "cfd351548cd2b58b802d52abdc8344d0",
+    "url": "/tamago-sushi-bar/static/media/matcha-tea.cfd35154.jpg"
+  },
+  {
+    "revision": "948a6754382207c9c347535b9afa227e",
+    "url": "/tamago-sushi-bar/static/media/menu-drink.948a6754.jpg"
+  },
+  {
+    "revision": "37d142d5e8762298dbe30d1cd34da88c",
+    "url": "/tamago-sushi-bar/static/media/menu-rice.37d142d5.jpg"
+  },
+  {
+    "revision": "cea9ff2c83e8d55cfe72e09dc28cf0fa",
+    "url": "/tamago-sushi-bar/static/media/menu-salad.cea9ff2c.jpg"
+  },
+  {
+    "revision": "40fd1df3978c88ef0d54df79595843b6",
+    "url": "/tamago-sushi-bar/static/media/menu-sushi.40fd1df3.jpg"
+  },
+  {
+    "revision": "36fce8fe065cee0b4aa74f5912907bc5",
+    "url": "/tamago-sushi-bar/static/media/mochi.36fce8fe.jpg"
+  },
+  {
+    "revision": "490e045d00a7e821c48ee4d389865fa2",
+    "url": "/tamago-sushi-bar/static/media/pudding.490e045d.jpg"
+  },
+  {
+    "revision": "3bc6df5ddd36ba839acb62d96574a32c",
+    "url": "/tamago-sushi-bar/static/media/reservation.3bc6df5d.jpg"
+  },
+  {
+    "revision": "5de0e06f39cdc146e73086c4e43c7e6c",
+    "url": "/tamago-sushi-bar/static/media/shiitake.5de0e06f.jpg"
+  },
+  {
+    "revision": "d09042b365e69bd6cb6cad4cb0d70d91",
+    "url": "/tamago-sushi-bar/static/media/soft.d09042b3.jpg"
+  },
+  {
+    "revision": "bce9652bdd3a526465c92d4b553942da",
+    "url": "/tamago-sushi-bar/static/media/tako.bce9652b.jpeg"
+  },
+  {
+    "revision": "2229b7b0fab39f02320b0a41523bb7b9",
+    "url": "/tamago-sushi-bar/static/media/tamago.2229b7b0.png"
+  },
+  {
+    "revision": "7eab428a68fcccd8763df44042497fc2",
+    "url": "/tamago-sushi-bar/static/media/testimonial.7eab428a.jpg"
+  },
+  {
+    "revision": "fd37a62d1ea65b58f2ab0793e3f31762",
+    "url": "/tamago-sushi-bar/static/media/tiramisu.fd37a62d.jpg"
+  },
+  {
+    "revision": "193dcdcc8c3ac9af7981e60925297cbd",
+    "url": "/tamago-sushi-bar/static/media/unagi.193dcdcc.jpg"
+  }
+]);
